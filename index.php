5e14d8d2a886aeffa0427f294ad6c595bea2893c
9ter <kasiyapo@gmail.com>
2023-08-12 00:34:53 +0700

    1

--- index.php
+++ index.php
@@ -11,14 +11,18 @@ if (!isset($_SESSION['user'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Smart Farm</title>
 </head>
+
 <body>
 
-<iframe src="https://snapshots.raintank.io/dashboard/snapshot/KhNtomPEu02bbGyKDsEM67hUfE7oWYWO?orgId=2" frameborder="0"></iframe>
-  
+  <iframe src="https://snapshots.raintank.io/dashboard/snapshot/KhNtomPEu02bbGyKDsEM67hUfE7oWYWO?orgId=2"
+    frameborder="0"></iframe>
+
 </body>
+
 </html>
