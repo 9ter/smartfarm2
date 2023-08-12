88418c789fc7c9a339d46a0288b2bf4b5a8b5b4b
9ter <kasiyapo@gmail.com>
2023-08-12 14:59:40 +0700

    1

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-session_start();
+include 'pages/config.php';
 
 if (!isset($_SESSION['user'])) {
   header("Location: pages/login.html");
@@ -11,13 +10,38 @@ if (!isset($_SESSION['user'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/main.css?v=1.5">
+
+  <link rel="icon" href="assets/img/logo.png" type="image/x-icon">
+
   <title>Smart Farm</title>
 </head>
-<body>
 
-  
+<body>
+  <h1>Sensor Measurement</h1>
+  <p>Current Temperature: <span id="temperatureValue">--</span> °C</p>
+  <button id="measureButton">Measure Temperature</button>
+
+  <script>
+    const measureButton = document.getElementById('measureButton');
+    const temperatureValue = document.getElementById('temperatureValue');
+
+    measureButton.addEventListener('click', () => {
+      const measuredTemperature = measureTemperature(); // Replace with actual measurement function
+      temperatureValue.textContent = measuredTemperature.toFixed(2); // Display the measurement
+    });
+
+    function measureTemperature() {
+      // Replace this with your actual measurement logic
+      // For now, let's simulate a random temperature between 20 and 30 degrees Celsius
+      const randomTemperature = Math.random() * 10 + 20;
+      return randomTemperature;
+    }
+  </script>
 </body>
+
 </html>
--- pages/config.php
+++ pages/config.php
@@ -1,5 +1,11 @@
 <?php
+
+// กำหนดอายุเซสชันให้เป็น 1 ชั่วโมง
+ini_set('session.gc_maxlifetime', 3600);
+
+// เรียกใช้ฟังก์ชัน session_start() และเริ่มต้นเซสชัน
 session_start();
+
 $servername = "localhost";
 $username = "thanankornm";
 $password = "BrqHAmTC3w_ZRCut";
