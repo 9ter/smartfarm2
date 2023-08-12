87069641bf409c51fb414c42c98d6854d2fa25c5
9ter <kasiyapo@gmail.com>
2023-08-12 19:59:06 +0700

    1

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ include 'pages/config.php';
 if (!isset($_SESSION['user'])) {
   header("Location: pages/login.html");
   exit();
+}else{
+
+  header("Location: ");
 }
 
 ?>
@@ -14,34 +17,33 @@ if (!isset($_SESSION['user'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- css -->
   <link rel="stylesheet" href="assets/css/main.css?v=1.5">
+  <link rel="stylesheet" href="assets/css/gauge.css?v=1.5">
+  <!--JS -->
+  <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="assets/js/GaugeMeter.js?v=1.0"></script>
+
+  <script>
+    $(document).ready(function () {
+      $(".GaugeMeter").gaugeMeter();
+    });
+
+    $("#GaugeMeter_1").gaugeMeter({ percent: 15 });
+  </script>
+
 
   <link rel="icon" href="assets/img/logo.png" type="image/x-icon">
 
   <title>Smart Farm</title>
+
+
 </head>
 
 <body>
   <h1>Sensor Measurement</h1>
-  <p>Current Temperature: <span id="temperatureValue">--</span> °C</p>
-  <button id="measureButton">Measure Temperature</button>
-
-  <script>
-    const measureButton = document.getElementById('measureButton');
-    const temperatureValue = document.getElementById('temperatureValue');
-
-    measureButton.addEventListener('click', () => {
-      const measuredTemperature = measureTemperature(); // Replace with actual measurement function
-      temperatureValue.textContent = measuredTemperature.toFixed(2); // Display the measurement
-    });
-
-    function measureTemperature() {
-      // Replace this with your actual measurement logic
-      // For now, let's simulate a random temperature between 20 and 30 degrees Celsius
-      const randomTemperature = Math.random() * 10 + 20;
-      return randomTemperature;
-    }
-  </script>
+  <div class="GaugeMeter" id="GaugeMeter_1"></div>
 </body>
 
 </html>
